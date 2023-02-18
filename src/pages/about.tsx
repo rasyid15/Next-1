f8de8164ad73f4eed80cd2ff6d24080c723fe522
Muhammad Rasyid Ridho <99931023+rasyid15@users.noreply.github.com>
2023-02-18 21:33:39 +0700

    Add files via upload

---
+++ src/pages/about.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+function about() {
+  return (
+    <div className="bg-black min-h-screen">
+      <div className="max-w-3xl mx-auto py-16">
+        <h1 className="text-4xl font-bold mb-4">Welcome to the About Page</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <Link href="/" className="text-blue-500 hover:text-blue-700">Home</Link>
+            <li>
+              <Link href="/about" className="text-blue-500 hover:text-blue-700">About</Link>
+            </li>
+            <li>
+              <Link href="/portofolio" className="text-blue-500 hover:text-blue-700">Portofolio</Link>
+            </li>
+            <li>
+              <Link href="/contact" className="text-blue-500 hover:text-blue-700"> Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    )
+}
+
+export default about
